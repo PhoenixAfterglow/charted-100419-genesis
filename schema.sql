836e8a21d45158133f2dc2c88cf9deb894466483
Johnny Maravelis <johnny@phoenixafterglow.com>
2019-10-09 20:48:49 -0400

    database work recap: sequelize models for User, Chart

--- schema.sql
+++ schema.sql
@@ -3,16 +3,18 @@ DROP DATABASE IF EXISTS chartedDB;
 -- Creates the "chartedDB" database --
 CREATE DATABASE chartedDB;
 
-CREATE TABLE charted_graph (
+CREATE TABLE charted_chart (
 	id INT NOT NULL AUTO_INCREMENT,
-	graph_name VARCHAR(255) NOT NULL,
+	-- chart_name VARCHAR(255) NOT NULL,
+	-- chart_values VARCHAR (100),
 	PRIMARY KEY (id),
 	FOREIGN KEY (charted_user_id) REFERENCES charted_user(id)
 );
 
 CREATE TABLE charted_data (
 	id INT NOT NULL AUTO_INCREMENT,
-	column_name VARCHAR(255) NOT NULL,
+	column_one_name VARCHAR(100) NOT NULL,
+	column_two_name VARCHAR(100) NOT NULL,
 	PRIMARY KEY (id),
 	FOREIGN KEY (charted_graph_id) REFERENCES charted_graph(id)
 );
