7cf75e636a6b358f8506e1d2ebee74471eadacf4
Ibrahim Yunus <younis.ib@gmail.com>
2019-10-10 13:18:15 -0400

    graph data from local csv file

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,107 @@ $(document).ready(function() {
     $.get("/api/user_data").then(function(data) {
         $(".member-name").text(data.username);
     });
+
+    /* CHART
+    ---------------------- */
+
+    async function setup() {
+        const chickenDatas = await getDatas();
+        const ctx = document.getElementById('chart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: chickenDatas.xs,
+                datasets: [{
+                    label: '108 5mo',
+                    data: chickenDatas.ys108,
+                    fill: false,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }, {
+                    label: '97 5mo',
+                    data: chickenDatas.ys97,
+                    fill: false,
+                    backgroundColor: 'rgba(167,105,0,0.4)',
+                    borderColor: 'rgb(167, 105, 0)',
+                    borderWidth: 1
+                }, {
+                    label: '120 6mo',
+                    data: chickenDatas.ys120,
+                    fill: false,
+                    backgroundColor: 'rgba(220,220,220,0.2)',
+                    borderColor: 'rgba(220,220,220,1)',
+                    borderWidth: 1
+                }, {
+                    label: '111 6mo',
+                    data: chickenDatas.ys111,
+                    fill: false,
+                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }, {
+                    label: '20 7mo',
+                    data: chickenDatas.ys20,
+                    fill: false,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                    borderColor: 'rgba(75, 192, 192, 1)',
+                    borderWidth: 1
+                }, {
+                    label: '15 7mo',
+                    data: chickenDatas.ys15,
+                    fill: false,
+                    backgroundColor: 'rgba(153, 102, 255, 0.2)',
+                    borderColor: 'rgba(153, 102, 255, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+    /*
+    -----------------------  */
+
+    async function getDatas() {
+        const xs = [];
+        const ys108 = [];
+        const ys97 = [];
+        const ys120 = [];
+        const ys111 = [];
+        const ys20 = [];
+        const ys15 = [];
+        const response = await fetch('../datas/Grass_Fed_Chickens_grass.csv');
+        const datas = await response.text();
+        console.log(datas);
+        const table = datas.split('\n').slice(1);
+        table.forEach(row => {
+            const columns = row.split(',');
+            const months = columns[0];
+            xs.push(months);
+            const _108_5mo = columns[1];
+            ys108.push(_108_5mo);
+            const _97_5mo = columns[2];
+            ys97.push(_97_5mo);
+            const _120_6mo = columns[3];
+            ys120.push(_120_6mo);
+            const _111_6mo = columns[4];
+            ys111.push(_111_6mo);
+            const _20_7mo = columns[5];
+            ys20.push(_20_7mo);
+            const _15_7mo = columns[6];
+            ys15.push(_15_7mo);
+            console.log(months, _108_5mo, _97_5mo, _120_6mo, _111_6mo, _20_7mo, _15_7mo);
+        });
+        return { xs, ys108, ys97, ys120, ys111, ys20, ys15 };
+    }
+
+    setup();
 });
