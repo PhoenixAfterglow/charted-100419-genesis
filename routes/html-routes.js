22b95e0af203a130c6cf42d15fefbb46e8310921
Ibrahim Yunus <younis.ib@gmail.com>
2019-10-13 12:01:16 -0400

    upload functionality added and still in test

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,11 @@
 // Requiring path to so we can use relative routes to our HTML files
-var path = require("path");
+const path = require("path");
+const multer = require("multer");
+const upload = multer({ dest: 'public/datas/' });
+const fs = require("fs");
 
 // Requiring our custom middleware for checking if a user is logged in
-var isAuthenticated = require("../config/middleware/isAuthenticated");
+const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
 
@@ -33,6 +36,33 @@ module.exports = function(app) {
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
     app.get("/members", isAuthenticated, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/members.html"));
+
+    });
+
+    /* POST saveblog router. */
+    app.post('/members', isAuthenticated, upload.single('uploadCsvDatas'), function(req, res, next) {
+
+        console.log(req.file.filename);
+        const uploadedFile = req.file.filename;
+
+        new Promise((resolve, reject) => {
+
+            fs.readFile(`public/datas/${uploadedFile}`, 'utf8',
+                function(err, datas) {
+                    if (err) reject(err);
+
+                    console.log(datas);
+
+
+
+
+                });
+
+            return resolve()
+        }).catch(console.log);
+
+
         res.sendFile(path.join(__dirname, "../public/members.html"));
     });
 
